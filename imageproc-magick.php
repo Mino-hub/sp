84fb8fa38889a2f03d273d47dc25a30e0d312a4c
pecodrive <pecodrive@gmail.co>
2016-10-21 20:36:11 +0900

    class

--- imageproc-magick.php
+++ imageproc-magick.php
@@ -5,31 +5,34 @@ $sourse[1]["sourse"]   = "http://i.imgur.com/9KySaHr.jpg";
 $sourse[1]["filename"] = "111dbc9dddda20c822900acdeffea730";
 $savepath = "";
 $limit_size = 800;
+$cq = 40;
 
 foreach ($sourse as $value) {
-    $image = new ImageScaleProc($value["sourse"], $value["filename"], $limit_size, $savepath);
+    $image = new ImageScaleProc($value["sourse"], $value["filename"], $limit_size, $savepath, $cq);
     unset($image);
 }
 
-class ImageScaleProc{
+class ImageScaleProcFunc{
     private $sourse;
     private $filename;
+    private $cq;
     private $image;
     private $image_info;
     private $limit_size;
     private $ext;
-    public function __construct($sourse, $filename, $limit_size, $savepath)
+    public function __construct($sourse, $filename, $limit_size, $savepath, $cq)
     {
         $this->sourse     = $sourse;
         $this->filename   = $filename;
         $this->savepath   = $savepath;
+        $this->savepath   = $cq;
         $this->image      = new Imagick($this->sourse);
         $this->image_info = $this->image->identifyImage();
         $this->limit_size = $limit_size;
         $this->extOutput();
         $this->compressQy();
-        // $this->scaleProc();
-        // $this->saveImage();
+        $this->scaleProc();
+        $this->saveImage();
     }
     private function extOutput(){
         if($this->image_info["mimetype"] === "image/jpeg"){
@@ -44,8 +47,8 @@ class ImageScaleProc{
     }
     private function compressQy(){
         $cq = $this->image->getImageCompressionQuality();
-        if($cq > 40){
-            $this->image->setImageCompressionQuality(40);
+        if($cq > $this->cq){
+            $this->image->setImageCompressionQuality($this->cq);
         }else{
             $this->image->setImageCompressionQuality($cq);
         }
