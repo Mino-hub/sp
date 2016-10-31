6ebde30eb6d45b8fb8e750390c73bea8cde13202
pecodrive <pecodrive@gmail.co>
2016-10-31 14:52:50 +0900

    curl

--- curltest.php
+++ curltest.php
@@ -3,8 +3,8 @@ $url = "http://hayabusa.open2ch.net/livejupiter/";
 $context = curl_init();
 curl_setopt($context, CURLOPT_URL, $url);
 curl_setopt($context, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($context, CURLOPT_PROXYPORT, 9050);//アクセスポート
-curl_setopt($context, CURLOPT_PROXY, "127.0.0.1");//IPアドレスかURL
+// curl_setopt($context, CURLOPT_PROXYPORT, 9050);//アクセスポート
+// curl_setopt($context, CURLOPT_PROXY, "127.0.0.1");//IPアドレスかURL
 $html = curl_exec($context);
 $info = curl_getinfo($context);
 curl_close($context);
