90e72199842a7478f4df3630e02c987bc092d77e
pecodrive <pecodrive@gmail.co>
2016-10-31 14:43:00 +0900

    curl

--- curltest.php
+++ curltest.php
@@ -6,4 +6,7 @@ curl_setopt($context, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_PROXYPORT, 9050);//アクセスポート
 curl_setopt($ch, CURLOPT_PROXY, "localhost");//IPアドレスかURL
 $html = curl_exec($context);
-var_dump($html);
+curl_close($context);
+$info = curl_getinfo($context);
+var_dump($info);
+// var_dump($html);
