c0db4d48e9053d99cedfa51047ebe1b2cbe46e2c
pecodrive <pecodrive@gmail.co>
2016-11-02 11:37:42 +0900

    net

--- curltest.php
+++ curltest.php
@@ -3,7 +3,6 @@ $url = "http://hayabusa.open2ch.net/livejupiter/";
 $context = curl_init();
 curl_setopt($context, CURLOPT_URL, $url);
 curl_setopt($context, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($context, CURLOPT_COOKIEFILE, 'cookie');
 curl_setopt($context, CURLOPT_COOKIE, '');
 curl_setopt($context, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
 curl_setopt($context, CURLOPT_PROXY, '172.20.0.31:9050');
