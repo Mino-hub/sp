23f98beb2d8ac48dc28765902892c36d978a577b
pecodrive <pecodrive@gmail.co>
2016-11-02 13:37:21 +0900

    net

--- curltest.php
+++ curltest.php
@@ -3,7 +3,7 @@ $url = "http://hayabusa.open2ch.net/livejupiter/";
 // $url = "http://open.open2ch.net/menu/";
 $context = curl_init();
 curl_setopt($context, CURLOPT_URL, $url);
-curl_setopt($context, CURLOPT_HTTPPROXYTUNNEL, TRUE);
+curl_setopt($context, CURLOPT_HTTPPROXYTUNNEL, true);
 // curl_setopt($context, CURLOPT_HEADER, true);
 // curl_setopt($context, CURLOPT_HTTPHEADER, ["User-Agent: curl/7.38.0"]);
 // curl_setopt($context, CURLOPT_RETURNTRANSFER, true);
