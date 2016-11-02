e3fea74f37fc6f111a19e0a7e976d4037a533cc8
pecodrive <pecodrive@gmail.co>
2016-11-02 14:14:50 +0900

    net

--- curltest.php
+++ curltest.php
@@ -4,15 +4,16 @@ $url = "http://hayabusa.open2ch.net/livejupiter/";
 $context = curl_init();
 curl_setopt($context, CURLOPT_URL, $url);
 curl_setopt($context, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($context, CURLINFO_HEADER_OUT,true);
+curl_setopt($context, CURLINFO_HEADER_OUT, true);
+curl_setopt($context, CURLOPT_AUTOREFERER, true);
 
-curl_setopt($context, CURLOPT_HEADER, true);
-curl_setopt($context, CURLOPT_HTTPHEADER, ["User-Agent: curl/7.38.0"]);
+// curl_setopt($context, CURLOPT_HEADER, true);
+// curl_setopt($context, CURLOPT_HTTPHEADER, ["User-Agent: curl/7.38.0"]);
 
 curl_setopt($context, CURLOPT_HTTPPROXYTUNNEL, true);
 curl_setopt($context, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
-// curl_setopt($context, CURLOPT_PROXY, '172.20.0.31:9050');
-curl_setopt($context, CURLOPT_PROXY, '127.0.0.1:9050');
+curl_setopt($context, CURLOPT_PROXY, '172.20.0.31:9050');
+// curl_setopt($context, CURLOPT_PROXY, '127.0.0.1:9050');
 
 curl_setopt($context, CURLOPT_COOKIEJAR, 'c');
 
