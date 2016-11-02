9eaf7258162d442344c0ea17ab52ca3bb35040d1
pecodrive <pecodrive@gmail.co>
2016-11-02 10:48:23 +0900

    net

--- curltest.php
+++ curltest.php
@@ -5,7 +5,7 @@ curl_setopt($context, CURLOPT_URL, $url);
 curl_setopt($context, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($context, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
 curl_setopt($context, CURLOPT_PROXY, '172.20.0.31:9050');
-curl_setopt($context, CURLOPT_COOKIEFILE, '');
+curl_setopt($context, CURLOPT_COOKIEJAR, 'cookie.txt');
 $html = curl_exec($context);
 $info = curl_getinfo($context);
 curl_close($context);
