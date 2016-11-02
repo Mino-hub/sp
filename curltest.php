0c4ca3d31c18dd5b814bb09c00be3df3604c7275
pecodrive <pecodrive@gmail.co>
2016-11-02 12:09:04 +0900

    net

--- curltest.php
+++ curltest.php
@@ -1,9 +1,19 @@
 <?php
 // $url = "http://hayabusa.open2ch.net/livejupiter/";
 $url = "http://open.open2ch.net/menu/";
+$headers = array(
+    "HTTP/1.0",
+    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Encoding:gzip ,deflate",
+    "Accept-Language:ja,en-us;q=0.7,en;q=0.3",
+    "Connection:keep-alive",
+    "cookie:",
+    "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:26.0) Gecko/20100101 Firefox/26.0"
+);
 $context = curl_init();
 curl_setopt($context, CURLOPT_URL, $url);
-curl_setopt($context, CURLOPT_HEADER, true);
+// curl_setopt($context, CURLOPT_HEADER, true);
+curl_setopt($context, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($context, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($context, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
 curl_setopt($context, CURLOPT_PROXY, '172.20.0.31:9050');
