ac3988600d85414811b8fbde472d3c8753a9fc53
pecodrive <pecodrive@gmail.co>
2016-11-02 12:01:30 +0900

    net

--- curltest.php
+++ curltest.php
@@ -3,12 +3,12 @@
 $url = "http://open.open2ch.net/menu/";
 $context = curl_init();
 curl_setopt($context, CURLOPT_URL, $url);
+curl_setopt($context, CURLOPT_HEADER, true);
 curl_setopt($context, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($context, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
 curl_setopt($context, CURLOPT_PROXY, '172.20.0.31:9050');
 curl_setopt($context, CURLOPT_COOKIEFILE, 'cookie_menu');
 curl_setopt($context, CURLOPT_COOKIEJAR, 'cookie_menu');
-curl_setopt($context, CURLOPT_COOKIE, 'cookie_menu');
 $html = curl_exec($context);
 $info = curl_getinfo($context);
 curl_close($context);
