8a30530c626fa6e57188a14125012234c3e2bd7f
pecodrive <pecodrive@gmail.co>
2016-11-02 14:58:43 +0900

    net

--- curltest.php
+++ curltest.php
@@ -1,27 +1,27 @@
 <?php
-$url = "socks5://hayabusa.open2ch.net/livejupiter/";
+$url = "http://hayabusa.open2ch.net/livejupiter/";
 // $url = "http://qiita.com/snize/items/fad7f4451d19903f8ac9";
-$context = curl_init();
-curl_setopt($context, CURLOPT_URL, $url);
-curl_setopt($context, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($context, CURLINFO_HEADER_OUT, true);
-// curl_setopt($context, CURLOPT_AUTOREFERER, true);
+// $context = curl_init();
+// curl_setopt($context, CURLOPT_URL, $url);
+// curl_setopt($context, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($context, CURLINFO_HEADER_OUT, true);
+// // curl_setopt($context, CURLOPT_AUTOREFERER, true);
+//
+// // curl_setopt($context, CURLOPT_HEADER, true);
+// // curl_setopt($context, CURLOPT_HTTPHEADER, ["User-Agent: curl/7.38.0"]);
+//
+// curl_setopt($context, CURLOPT_HTTPPROXYTUNNEL, true);
+// // curl_setopt($context, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+// curl_setopt($context, CURLOPT_PROXY, 'socks5://172.20.0.31:9050');
+//
+// curl_setopt($context, CURLOPT_COOKIEJAR, 'c');
+//
+// $html = curl_exec($context);
+// $info = curl_getinfo($context);
+// curl_close($context);
+// var_dump($info);
 
-// curl_setopt($context, CURLOPT_HEADER, true);
-// curl_setopt($context, CURLOPT_HTTPHEADER, ["User-Agent: curl/7.38.0"]);
-
-curl_setopt($context, CURLOPT_HTTPPROXYTUNNEL, true);
-// curl_setopt($context, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
-curl_setopt($context, CURLOPT_PROXY, 'socks5://172.20.0.31:9050');
-
-curl_setopt($context, CURLOPT_COOKIEJAR, 'c');
-
-$html = curl_exec($context);
-$info = curl_getinfo($context);
-curl_close($context);
-var_dump($info);
-
-// $exec = exec("curl -c --socks5 127.0.0.1:9050 http://hayabusa.open2ch.net/livejupiter/",$output);
-// var_dump($exec);
-// var_dump($output);
+$exec = shell_exec("curl -c --socks5 172.20.0.31:9050 http://hayabusa.open2ch.net/livejupiter/");
+var_dump($exec);
+var_dump($output);
 
