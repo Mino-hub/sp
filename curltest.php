6c8d33bd9480eff654b4c4f8daf5b30e36725ef4
pecodrive <pecodrive@gmail.co>
2016-11-02 13:19:56 +0900

    net

--- curltest.php
+++ curltest.php
@@ -7,7 +7,7 @@ curl_setopt($context, CURLOPT_URL, $url);
 // curl_setopt($context, CURLOPT_HEADER, true);
 // curl_setopt($context, CURLOPT_HTTPHEADER, ["User-Agent: curl/7.38.0"]);
 // curl_setopt($context, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($context, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+curl_setopt($context, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
 curl_setopt($context, CURLOPT_PROXY, '172.20.0.31:9050');
 // curl_setopt($context, CURLOPT_COOKIEJAR, 'cookie_menu');
 curl_setopt($context, CURLINFO_HEADER_OUT,true);
