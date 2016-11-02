4151d2d0b1b4e71adba89dd5959d2dde4cd7a0eb
pecodrive <pecodrive@gmail.co>
2016-11-02 11:47:24 +0900

    net

--- curltest.php
+++ curltest.php
@@ -1,12 +1,13 @@
 <?php
-// $url = "http://hayabusa.open2ch.net/livejupiter/";
-$url = "http://ipinfo.io";
+$url = "http://hayabusa.open2ch.net/livejupiter/";
 $context = curl_init();
 curl_setopt($context, CURLOPT_URL, $url);
 curl_setopt($context, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($context, CURLOPT_COOKIE, '');
 curl_setopt($context, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
 curl_setopt($context, CURLOPT_PROXY, '172.20.0.31:9050');
+curl_setopt($context, CURLOPT_COOKIEJAR, 'cookie');
+curl_setopt($context, CURLOPT_COOKIEFILE, 'cookie');
+curl_setopt($context, CURLOPT_COOKIE, 'cookie');
 $html = curl_exec($context);
 $info = curl_getinfo($context);
 curl_close($context);
